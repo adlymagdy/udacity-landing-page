1f55e35831309e720a71e2e320132ca3b45a9a39
Adly Magdy <1126032+adlymagdy@users.noreply.github.com>
2021-02-08 20:52:03 +0200

    - Add class 'active' to section when near top of viewport - Add helper function isInViewport - Clean code and remove repetitions

--- js/app.js
+++ js/app.js
@@ -2,25 +2,58 @@
  * Define Global Variables
  *
  */
-
 let navbar_list = document.querySelector("#navbar__list");
-let sections = document.querySelectorAll("main > section")
+let sections = document.querySelectorAll("main > section");
 
 /**
  * End Global Variables
  * Start Helper Functions
  *
  */
+function isInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+ */
 
 // build nav
-
-for (let section of sections) {
-   if (section.id) {
-       let section_content = section.querySelector(".landing__container");
-       let section_title = section_content.querySelector('h2').textContent;
-       let list_item = document.createElement('li');
-       list_item.innerHTML = `<a href=#${section.id}>${section_title}</a>`;
-       navbar_list.appendChild(list_item);
-   }
+function buildNav(section) {
+  if (section.id) {
+    let section_content = section.querySelector(".landing__container");
+    let section_title = section_content.querySelector('h2').textContent;
+    let list_item = document.createElement('li');
+    list_item.innerHTML = `<a href=#${section.id}>${section_title}</a>`;
+    navbar_list.appendChild(list_item);
+  }
 }
 
+
+// Add class 'active' to section when near top of viewport
+
+function addActive (section) {
+  document.addEventListener('scroll', function () {
+    if(isInViewport(section)) {
+      section.classList.add("your-active-class")
+    } else {
+      section.classList.remove("your-active-class")
+    }
+  })
+}
+
+// Scroll to anchor ID using scrollTO event
+
+
+
+for (let section of sections) {
+  buildNav(section);
+  addActive(section);
+}
